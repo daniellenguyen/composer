c3433f6e3851fa5189b0a29d37126375acfb82e1
Justin Hynes-Bruell <hynes-bruell.j@husky.neu.edu>
2016-04-19 11:16:53 -0400

    Add Note Functionality is Here
    It however will need to be tweaked to have the right spacing
    
    Bugs for my old Views:
    1. Adding Note that extends past the frame of the screen creates a VERY long note.
    2. Clicking and going the wrong direction creates no note at all.

--- src/cs3500/music/view/CompositeViewAdapter.java
+++ src/cs3500/music/view/CompositeViewAdapter.java
@@ -3,11 +3,13 @@ package cs3500.music.view;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
 import javax.sound.midi.InvalidMidiDataException;
 
 import cs3500.music.controller.KeyboardHandler;
 import cs3500.music.model.Note;
+import cs3500.music.model.NoteList;
 import cs3500.music.model.SoundUnit;
 import cs3500.music.model.SoundUnitList;
 import cs3500.music.model.SoundUnitListToIPlayerModelAdapter;
@@ -35,12 +37,71 @@ public class CompositeViewAdapter implements cs3500.music.view.ICompositeView {
   //TODO This is gonna be tricky
   @Override
   public Note NotePressed(Point mousePoint, SoundUnitList model) {
-    return new Note(SoundUnit.Pitch.C, SoundUnit.Octave.FOUR, 999, 1000);
+    System.out.println("NOTE PRESS ATTEMPTED");
+    return new Note(SoundUnit.Pitch.C, SoundUnit.Octave.FOUR, 998, 1000);
   }
 
   //TODO This is gonna be tricky
   @Override
   public Note SpacePressed(Point mousePoint, SoundUnitList model) {
+    int moveOverForBeat = model.getCurrentBeat() * 25;
+
+    System.out.println("X: " + mousePoint.getX() + " Y: " + mousePoint.getY());
+
+    //UpdateMousePoint (4/18/16)
+    //mousePoint.setLocation(mousePoint.getX(), mousePoint.getY()-45);
+
+    int separation = 15;
+
+    int rangeOfSong = model.getHighestNote().getMIDIPitch() -
+            model.getLowestNote().getMIDIPitch();
+    //For each Beat in the song Draws the Notes
+    for (int BeatNumber = 0; BeatNumber < model.songLength(); BeatNumber++) {
+      ArrayList<SoundUnit> ListOfNotesAtBeat = new ArrayList<>();
+      ListOfNotesAtBeat.addAll(model.getAllAtTime(BeatNumber));
+
+      //Create a Column of Notes
+      for (int i = rangeOfSong; i >= 0; i--) {
+        SoundUnit rangeNote = new Note(SoundUnit.Pitch.C, SoundUnit.Octave.FOUR, 0, 1);
+        rangeNote.setPitchAndOctaveFromMIDI(model.getHighestNote().getMIDIPitch() - i);
+
+        boolean noteStarts = false;
+        boolean noteContinues = false;
+
+        SoundUnitList PossibleSaveNote = new NoteList();
+
+
+
+        noteStarts = true;
+        rangeNote.setEnd(BeatNumber + 1);
+        rangeNote.setStart(BeatNumber);
+        PossibleSaveNote.add(rangeNote);
+
+        /*
+        //rangeNote.setStart();*/
+
+        //If a Note is Starting then Fill it! This takes Priority over the Continue
+        if (noteStarts) {
+          if (mousePoint.getX() > 40 + (25 * BeatNumber)
+                  - moveOverForBeat && mousePoint.getX() < 40
+                  + (25 * BeatNumber) - moveOverForBeat + 25) {
+            if (mousePoint.getY() > ((separation * i))
+                    + 15 && mousePoint.getY() < ((separation * i)) + 15 + 15) {
+              return PossibleSaveNote.getHighestNote();
+            }
+          }
+        } else if (noteContinues) {
+          if (mousePoint.getX() > 40 + (25 * BeatNumber)
+                  - moveOverForBeat && mousePoint.getX() < 40 + (25 * BeatNumber)
+                  - moveOverForBeat + 25) {
+            if (mousePoint.getY() > ((separation * i))
+                    + 15 && mousePoint.getY() < ((separation * i)) + 15 + 15) {
+              return PossibleSaveNote.getHighestNote();
+            }
+          }
+        }
+      }
+    }
     return new Note(SoundUnit.Pitch.C, SoundUnit.Octave.FOUR, 999, 1000);
   }
 
@@ -88,7 +149,7 @@ public class CompositeViewAdapter implements cs3500.music.view.ICompositeView {
     guiView.outputView();
     this.guiView.updateScroll("x", BeatNumber);
     try {
-      this.midiView.playBeat(BeatNumber); //TODO UNCOMMENT THIS TO PLAY SONG
+      this.midiView.playBeat(BeatNumber);
     } catch (InvalidMidiDataException e) {
       e.printStackTrace();
     }
